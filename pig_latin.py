9e7ed4251dce8216eabe6c66fdfd631c78b34540
vensder <vensder@localhost.localdomain>
2015-09-21 23:42:20 +0300

    transform word

--- pig_latin.py
+++ pig_latin.py
@@ -15,5 +15,16 @@ elif not username.isalpha():
 
 pyg = 'ay'
 
+original = raw_input('Enter a word:')
 
+if len(original) > 0 and original.isalpha():
+    print original
+else:
+    print 'empty'
+    
+word = original.lower()
+first = word[0]
+new_word = word + first + pyg
+new_word = new_word[1:len(new_word)]
 
+print new_word
