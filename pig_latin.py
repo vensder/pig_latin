ac1962ac2f42081815ec26ba3131176d568e438c
vensder <vensder@gmail.com>
2015-09-22 01:13:13 +0300

    transform word

--- pig_latin.py
+++ pig_latin.py
@@ -4,27 +4,36 @@
 print '--== Pig Latin (oo) Igpay Atinlay ==--'
 print 'Welcome to the Pig Latin Translator!'
 
-username = raw_input("Enter Your Name, Please:")
+def username():
+    uname = raw_input("Enter Your Name, Please:")
+    return (uname)
 
-if len(username) > 0 and username.isalpha():
-    print username
-elif len(username) == 0:
-    print "Please Try again. You should type your name and press Enter key"
-elif not username.isalpha():
-    print "Please don't use only alphabetical characters."
+def check_username(uname):
+    if len(uname) > 0 and uname.isalpha():
+        print "Hello " + uname + "!"
+    elif len(uname) == 0:
+        print "Please Try again. You should type your name and press Enter key"
+    elif not uname.isalpha():
+        print "Please use only alphabetical characters."
 
-pyg = 'ay'
+def pyg():
+    pyg = 'ay'
 
-original = raw_input('Enter a word:')
+    original = raw_input('Enter a word:')
 
-if len(original) > 0 and original.isalpha():
-    print original
-else:
-    print 'empty'
-    
-word = original.lower()
-first = word[0]
-new_word = word + first + pyg
-new_word = new_word[1:len(new_word)]
+    if len(original) > 0 and original.isalpha():
+        print original
+    else:
+        print 'empty'
+        
+    word = original.lower()
+    first = word[0]
+    new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
 
-print new_word
+    print new_word
+
+n = username()
+check_username(n)
+
+print 'End of code'
